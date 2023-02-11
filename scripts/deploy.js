243c2537e479e7437ceb6c566c2e2f38bb546fdd
Egor Gutrov <gutrov.egor@gmail.com>
2023-02-11 20:45:14 +0000

    Fixed identation

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,19 +1,19 @@
 async function main() {
-    const [deployer] = await ethers.getSigners();
-  
-    console.log("Deploying contracts with the account:", deployer.address);
-  
-    console.log("Account balance:", (await deployer.getBalance()).toString());
-  
-    const CHBSHToken = await ethers.getContractFactory("CHBSHToken");
-    const token = await CHBSHToken.deploy();
-  
-    console.log("Token address:", token.address);
-  }
-  
-  main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-      console.error(error);
-      process.exit(1);
-    });
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const CHBSHToken = await ethers.getContractFactory("CHBSHToken");
+  const token = await CHBSHToken.deploy();
+
+  console.log("Token address:", token.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
