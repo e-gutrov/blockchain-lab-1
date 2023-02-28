e68537d953f0ef60b7cf3b6e8a8936db16c9cb02
Egor Gutrov <gutrov.egor@gmail.com>
2023-02-28 21:55:42 +0000

    Edited contract

--- contracts/CHBSHToken.sol
+++ contracts/CHBSHToken.sol
@@ -5,6 +5,19 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract CHBSHToken is ERC20, Ownable {
+    struct Character {
+        address creator;
+        uint id;
+        string name;
+        bool is_real;
+    }
+
+    event CharacterCreated(address creator, uint id, string name);
+    event CharacterRemoved(address creator, uint id, string name);
+
+    uint curId;
+    mapping(uint => Character) public characters;
+
     function mint(address account, uint256 amount) public onlyOwner {
         _mint(account, amount);
     }
@@ -15,5 +28,23 @@ contract CHBSHToken is ERC20, Ownable {
 
     constructor() ERC20("Cheburashka Token", "CHBSH") {
         mint(msg.sender, 1000000);
+        curId = 0;
+    }
+
+    function createCharacter(string memory name, bool is_real) public {
+        characters[curId] = Character(msg.sender, curId, name, is_real);
+        curId += 1;
+
+        emit CharacterCreated(msg.sender, curId - 1, name);
+    }
+
+    function removeCharacter(uint id) public {
+        // Not sure if it should be storage (as it's stored
+        // on blockchain) or memory (as we delete it)
+        Character memory character = characters[id];
+        require(msg.sender == character.creator);
+        emit CharacterRemoved(msg.sender, id, character.name);
+
+        delete characters[id];
     }
 }
